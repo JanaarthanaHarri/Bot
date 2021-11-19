195557b26ef168b36e8110d6d96900050e8b55e1
jana <janaaharri97@gmail.com>
2021-11-18 22:01:05 -0800

    First commit

---
+++ db_script.py
@@ -0,0 +1,22 @@
+import sqlite3
+connection = sqlite3.connect('data.db')
+
+cursor = connection.cursor()
+
+cursor.execute('''CREATE TABLE orders
+                (order_date, order_number, order_email, brand, model, status)''')
+
+orders = [('2021-11-18', 123,'tony@gmail.com','iphone',12,'shipped')]
+
+cursor.executemany('INSERT INTO orders VALUES (?,?,?,?,?,?)', orders)
+
+cursor.execute('''CREATE TABLE inventory
+                    (brand, model''')
+
+inventory = [(6, 'iphone'),(7, 'iphone'),(8, 'iphone'),(9, 'iphone')]
+
+cursor.executemany('INSERT INTO inventory VALUES (?,?)', inventory)
+
+connection.commit()
+
+connection.close()
