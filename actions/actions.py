18f7cb6d1a1593a6b363424f9e79bfec98f8d692
jana <janaaharri97@gmail.com>
2021-11-19 11:30:45 -0800

    form update

--- actions/actions.py
+++ actions/actions.py
@@ -1,27 +1,43 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Any, Text, Dict, List
 
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+import sqlite3
 
-# This is a simple example for a custom action which utters "Hello World!"
+path_to_db = "data.db"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+class OrderStatus(Action):
+    def name(self) -> Text:
+        return "action_order_status"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+
+        # connect to DB
+        connection = sqlite3.connect(path_to_db)
+        cursor = connection.cursor()
+
+        # get email slot
+        order_email = (tracker.get_slot("email"),)
+
+        # retrieve row based on email
+        cursor.execute("SELECT * FROM orders WHERE order_email=?", order_email)
+        data_row = cursor.fetchone()
+
+        if data_row:
+            # convert tuple to list
+            data_list = list(data_row)
+
+            # respond with order status
+            dispatcher.utter_message(template="utter_order_status", status=data_list[5])
+            connection.close()
+            return []
+        else:
+            # db didn't have an entry with this email
+            dispatcher.utter_message(template="utter_no_order")
+            connection.close()
+            return []
--- db_script.py
+++ db_script.py
@@ -4,16 +4,16 @@ connection = sqlite3.connect('data.db')
 cursor = connection.cursor()
 
 cursor.execute('''CREATE TABLE orders
-             (order_date, order_number, order_email, brand, model, status)''')
+             (order_date, order_number, order_email, food, qty, status)''')
 
-orders = [('2021-11-18', 123,'tony@gmail.com','iphone',12,'shipped')]
+orders = [('2021-11-18', 123,'tony@gmail.com','pizza',2,'cooking')]
 
 cursor.executemany('INSERT INTO orders VALUES (?,?,?,?,?,?)', orders)
 
 cursor.execute('''CREATE TABLE inventory
-                    (brand, model)''')
+                    (qty, food)''')
 
-inventory = [(6, 'iphone'),(7, 'iphone'),(8, 'iphone'),(9, 'iphone')]
+inventory = [(3, 'pizza'),(3, 'boba'),(1, 'biriyani'),(1, 'ramen')]
 
 cursor.executemany('INSERT INTO inventory VALUES (?,?)', inventory)
 
